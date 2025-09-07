1744dc7ea519923d870e4ceb11ae463cdf1f3c76
Pr1mary <raflisatriandi@outlook.co.id>
2025-09-07 17:25:07 +0700

    DONE using yml config for configuration variables instead of env

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ from datetime import datetime
 import pytz
 from pathlib import Path
 import socket
+import yaml
 
-load_dotenv()
+config = {}
+with open("config.yml", 'r') as stream:
+    config = yaml.safe_load(stream)
 
 def getCurrTime():
     utc_now = datetime.now(pytz.utc)
-    zone = pytz.timezone(os.getenv("TIME_ZONE"))
+    zone = pytz.timezone(config.get("timezone"))
 
     return utc_now.astimezone(zone)
 
@@ -23,19 +26,18 @@ def printLog(args):
 def dataStruct(machine_id):
     data = {
         "last_update": getCurrTime(),
-        "status": "UP",
-        "interval_min": int(os.getenv("INTERVAL_MINUTES")),
+        "interval_min": int(config.get("interval_minutes")),
         "machine_id": machine_id,
-        "group_id": os.getenv("GROUP_ID")
+        "group_id": config.get("group_id")
     }
     return data
 
 def uptimeStart():
-    cred = credentials.Certificate(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+    cred = credentials.Certificate(config.get("firebase_creds"))
     app = firebase_admin.initialize_app(cred)
     db = firestore.client(app)
 
-    machine_id = os.getenv("INSTANCE_ID") or socket.gethostname()
+    machine_id = config.get("instance_id") or socket.gethostname()
 
     printLog("Start write uptime")
     db.collection("machine-uptime").document(machine_id).set(dataStruct(machine_id))
@@ -45,7 +47,7 @@ def registerCron():
 
     cron = CronTab(user="root")
     job = cron.new(command="python3 {} start >> {}".format(Path(__file__).absolute(), Path("log.txt").absolute()))
-    job.minute.every(int(os.getenv("INTERVAL_MINUTES")))
+    job.minute.every(int(config.get("interval_minutes")))
     cron.write()
 
     printLog("Script has been registered to crontab")

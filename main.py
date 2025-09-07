e56818c4ae1563f69e29be4a3f335e68da03ddbf
Pr1mary <raflisatriandi@outlook.co.id>
2025-09-07 23:45:30 +0700

    DONE adding local and public ip to data structure, FIX cronjob for registering and removing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
 import sys
 import firebase_admin
 from firebase_admin import firestore, credentials
-from dotenv import load_dotenv
 from crontab import CronTab
 from datetime import datetime
 import pytz
@@ -10,31 +8,63 @@ from pathlib import Path
 import socket
 import yaml
 import getpass
+import requests
 
 config = {}
-with open("config.yml", 'r') as stream:
+project_path = Path(__file__).absolute().parent
+with open("{}/config.yml".format(project_path), 'r') as stream:
     config = yaml.safe_load(stream)
 
+# get current time with timezone from config
 def getCurrTime():
     utc_now = datetime.now(pytz.utc)
     zone = pytz.timezone(config.get("timezone"))
 
     return utc_now.astimezone(zone)
 
+# simple log printer with timestamp
 def printLog(args):
     print("({}) {}".format(getCurrTime(), args))
 
+# fetch local ip address using socket
+def fetchLocalIP():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        s.connect(('8.8.8.8', 1)) # connect to google dns
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+def fetchPublicIP():
+    try:
+        ip = requests.get('https://api.ipify.org').text
+    except Exception as e:
+        ip = None
+    return ip
+
+# create data structure for firestore
 def dataStruct(machine_id):
     data = {
         "last_update": getCurrTime(),
         "interval_min": int(config.get("interval_minutes")),
         "machine_id": machine_id,
-        "group_id": config.get("group_id")
+        "group_id": config.get("group_id"),
+        "local_ip": fetchLocalIP(),
+        "public_ip": fetchPublicIP()
     }
     return data
 
+# command "start" - write uptime data to firestore
 def uptimeStart():
-    cred = credentials.Certificate(config.get("firebase_creds"))
+    firebase_creds = "{}/{}".format(project_path, config.get("firebase_creds"))
+    if not Path(firebase_creds).exists():
+        printLog("Firebase credentials not found!")
+        return
+    cred = credentials.Certificate(firebase_creds)
     app = firebase_admin.initialize_app(cred)
     db = firestore.client(app)
 
@@ -44,15 +74,17 @@ def uptimeStart():
     db.collection("machine-uptime").document(machine_id).set(dataStruct(machine_id))
     printLog("Write uptime done")
 
+# command "register" - register this script to cronjob
 def registerCron():
     user = getpass.getuser()
     cron = CronTab(user=user)
+    virtualenv_path = "{}/{}".format(project_path, config.get("virtualenv")) if config.get("virtualenv") else None
 
-    if config.get("virtualenv"):
-        command = "source {}/bin/activate && python3 {} start >> {}".format(
-            Path(config.get("virtualenv")).absolute(),
+    if virtualenv_path and Path(virtualenv_path).exists():
+        command = "{}/bin/python3 {} start >> {}".format(
+            virtualenv_path,
             Path(__file__).absolute(),
-            Path("log.txt").absolute())
+            "{}/{}".format(project_path, "log.txt"))
     else:
         command = "python3 {} start >> {}".format(Path(__file__).absolute(), Path("log.txt").absolute())
 
@@ -62,22 +94,22 @@ def registerCron():
 
     printLog("Script has been registered to crontab")
 
+# command "remove" - remove this script from cronjob
 def removeCron():
     user = getpass.getuser()
     cron = CronTab(user=user)
 
     cron.remove_all(comment="project-uptime")
+    cron.write()
     
     printLog("Script has been removed from crontab")
 
+# command "help" - show help
 def showHelp():
     print("Basic commands")
     print(" start\t\t: trigger update status to firestore")
     print(" register\t: register cron scheduling for this script")
     print(" remove\t\t: remove this script from cron scheduler")
-
-def debug():
-    print("{}/bin/activate".format(Path(config.get("virtualenv")).absolute()))
 
 def main():
     known_args = ["start", "register", "remove", "help", "debug"]
@@ -90,6 +122,5 @@ def main():
     elif sys.argv[1] == known_args[1]: registerCron()
     elif sys.argv[1] == known_args[2]: removeCron()
     elif sys.argv[1] == known_args[3]: showHelp()
-    elif sys.argv[1] == known_args[4]: debug()
 
 main()
